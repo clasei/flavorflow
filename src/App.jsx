aabcaf70d1c2e3faa92110f6c1eb9be3ff2f3fc9
clara seijo <clasei@proton.me>
2024-09-17 18:16:31 +0200

    day 1 done

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import './App.css'
 import Navbar from "./components/NavBar.jsx"
-import Footer from "./components/Footer.jsx"
+import Sidebar from "./components/Sidebar.jsx"
 
 function App() {
 
   return (
-    <>
+    <div id='appPage'>
       <Navbar />
-
-    </>
+      <div id='mainPage'>
+        <Sidebar />
+        <div>Contenido</div>
+      </div>
+    </div>
   )
 }
 
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,6 +1,12 @@
+import Footer from "./Footer"
+
 function Sidebar() {
   return (
     <div id="sidebar">
+      <section id="sidebarLinks">
+        <a href="Home">Home</a>
+        <a href="About">About Us</a>
+      </section>
       <Footer />
     </div>
   )
