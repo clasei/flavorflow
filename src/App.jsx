dce523e997369089c4a7ad14a9d71de239dd0361
Núria Soley <nuria.soley.93@gmail.com>
2024-09-19 15:35:32 +0200

    nadbar en sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import './App.css'
-import Navbar from "./components/NavBar.jsx"
 import Sidebar from "./components/Sidebar.jsx"
 import RecipeList from './components/RecipeList.jsx'
 import Dashboard from "./pages/Dashboard.jsx"
+import About from "./pages/About.jsx"
 import {Routes, Route} from "react-router-dom"
 
 
@@ -11,12 +11,12 @@ function App() {
   return (
     <>
     <div id='appPage'>
-      <Navbar />
     <div id='mainPage'>
         <Sidebar />
        
        <Routes>
           <Route path="/" element={<Dashboard/>}></Route>
+          <Route path="/About" element={<About/>}></Route>
        </Routes>
       </div>
     </div>
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,10 +2,12 @@ import Footer from "./Footer"
 import {Routes, Route, Link} from "react-router-dom"
 import Dashboard from "../pages/Dashboard.jsx"
 import About from "../pages/About.jsx"
+import Navbar from "./NavBar.jsx"
 
 function Sidebar() {
   return (
     <div id="sidebar">
+      <Navbar />
       <section id="sidebarLinks">
         <Link to="/" element={<Dashboard/>}>Home</Link>
         <Link to="/About" element={<About />}>About Us</Link>
