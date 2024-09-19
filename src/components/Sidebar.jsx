6692262b2746a37e4b668e552de1c3c91bda66f9
Ana Badolato <badolatoana@gmail.com>
2024-09-19 23:55:18 +0200

    Sidebar -in process-

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -4,16 +4,58 @@ import Dashboard from "../pages/Dashboard.jsx"
 import About from "../pages/About.jsx"
 import Navbar from "./NavBar.jsx"
 
+import profileImg from "../assets/icons/profile.svg";
+
+
 function Sidebar() {
   return (
-    <div id="sidebar">
-      <Navbar />
-      <section id="sidebarLinks">
-        <Link to="/" element={<Dashboard/>}>Home</Link>
-        <Link to="/About" element={<About />}>About Us</Link>
-      </section>
-      <Footer />
+
+    <div className="sidebar" id="">
+
+      <div className="sidebarIcons">
+        
+      </div>
+
+
+      <div className="sidebarText">
+
+        <section className="sidebarProfile">
+          <img className="profileImg" src={profileImg} alt="" />
+          <h2>Lira</h2>
+        </section>
+
+        <hr />
+
+        <section className="sidebarLinks">
+          <Link to="/" element={<Dashboard/>}>Home</Link>
+          <Link to="/About" element={<About />}>About Us</Link>
+        </section>
+
+        <hr />
+
+        <section className="sidebarCategories">
+          <p>Breakfast</p>
+          <p>Lunch</p>
+          <p>Snack</p>
+          <p>Dinner</p>
+        </section>
+
+        <hr />
+
+        <section className="sidebarFooter">
+          <a id="githubLink" href="https://github.com/clasei/flavorflow">FlavorFlow Repo</a>
+        </section>
+      </div>
+
     </div>
+    // <div id="sidebar">
+    //   <Navbar />
+    //   <section id="sidebarLinks">
+    //     <Link to="/" element={<Dashboard/>}>Home</Link>
+    //     <Link to="/About" element={<About />}>About Us</Link>
+    //   </section>
+    //   <Footer />
+    // </div>
   )
 }
 
