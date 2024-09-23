065c45fae19a5f057fc0b256aead8b97523e2770
Ana Badolato <badolatoana@gmail.com>
2024-09-24 01:42:39 +0200

    not found ready

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,6 +1,25 @@
+import { Link } from "react-router-dom";
+import error from "../assets/images/error.jpg";
+
 function NotFound() {
   return (
-    <div>NotFound</div>
+    <div className="mainContainer">
+      
+      <div className="mainBar">
+        <p>Page not found</p>
+      </div>
+        
+      <div class='aboutContainer'>
+  <h1>🥔 Oops! We overcooked the potatoes!</h1>
+  <span>It seems this page has turned into mashed potatoes. Let’s whisk you back to something tastier! 🍽️</span>
+  <img id='notFound' src={error} alt="error" />
+  <Link id="returnHome" to="/">
+          <p>Return Home</p>
+      </Link>
+
+</div>
+
+    </div>
   )
 }
 
